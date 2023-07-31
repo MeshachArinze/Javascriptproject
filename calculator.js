d9e5b6ddf8bacdad0ff66a84c46b5d2d6583a1a3
MeshachArinze <meshacharinze@gmail.com>
2023-07-31 11:11:12 +0100

    commit to repo

---
+++ calculator.js
@@ -0,0 +1,43 @@
+export default class Calculator {
+  constructor(
+    primaryOperandDisplay,
+    secondaryOperandDisplay,
+    operationDisplay
+  ) {
+    this.#primaryOperandDisplay = primaryOperandDisplay;
+    this.#secondaryOperandDisplay = secondaryOperandDisplay;
+    this.#operationDisplay = operationDisplay;
+
+    this.clear();
+  }
+
+  #primaryOperanDiplay;
+  #secondaryOperandDisplay;
+  #operandDisplay
+
+  get primaryOperand() {
+    return parseFloat(this.#primaryOperanDiplay.dataset.value);
+  }
+
+  set primaryOperand(value) {
+    this.#primaryOperanDiplay.dataset.value = value ?? ""
+    this.#primaryOperanDiplay.textContent = displayNumber(value);
+  }
+
+  get secondaryOperand() {
+    return parseFloat(this.#primaryOperanDiplay.dataset.value);
+  }
+
+  set secondaryOperand(value) {
+    this.#primaryOperanDiplay.dataset.value = value ?? "";
+    this.#primaryOperanDiplay.textContent = displayNumber(value);
+  }
+
+  get operation() {
+    return this.#operandDisplay.textContent
+  }
+
+  set operation(value) {
+    this.#operandDisplay.textContent = value ?? ""
+  }
+}
